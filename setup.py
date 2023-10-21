a4232ab2a00a96e7f6c2dbf535b61cce2bc0fa1f
Camille Ibsen <62849837+ibsenc@users.noreply.github.com>
2023-10-21 15:10:38 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from version import __version__ as version
 
 
@@ -6,8 +6,9 @@ setup(name='bq_helper',
       version=version,
       description='Helper class to simplify common read-only BigQuery tasks.',
       author='Sohier Dane',
-      url='https://github.com/SohierDane/BigQuery_Helper',
+      url='https://github.com/ibsenc/BigQuery_Helper',
       license='Apache 2.0',
       install_requires=['pandas', 'google-cloud-bigquery'],
-      classifiers=['Programming Language :: Python :: 3'],
+      classifiers=['Programming Language :: Python :: 3'], 
+      packages=find_packages(include=['bq_helper', 'test_helper'])
       )
